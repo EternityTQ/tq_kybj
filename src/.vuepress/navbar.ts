fcdc5795d69215a78c76918a500b5f6821eb4186
EternityTQ <1586049354@qq.com>
2024-03-21 10:56:20 +0800

    create files

---
+++ src/.vuepress/navbar.ts
@@ -0,0 +1,64 @@
+import { navbar } from "vuepress-theme-hope";
+
+export default navbar([
+  "/",
+  {
+    text: "考研数学",
+    icon: "lightbulb",
+    prefix: "math/",
+    children: [
+      {
+        text: "高数",
+        icon: "lightbulb",
+        link: "gaoshu/",
+      },
+      {
+        text: "线代",
+        icon: "lightbulb",
+        link: "xiandai/",
+      },
+      {
+        text: "概率论",
+        icon: "lightbulb",
+        link: "gailvlun/",
+      },
+    ],
+  },
+  {
+    text: "考研政治",
+    icon: "lightbulb",
+    link: "/political/",
+  },
+  {
+    text: "考研英语",
+    icon: "lightbulb",
+    link: "/english/",
+  },
+  {
+    text: "考研408",
+    icon: "lightbulb",
+    prefix: "/408/",
+    children: [
+      {
+        text: "数据结构",
+        icon: "lightbulb",
+        link: "sjjg/",
+      },
+      {
+        text: "操作系统",
+        icon: "lightbulb",
+        link: "czxt/",
+      },
+      {
+        text: "计算机组成原理",
+        icon: "lightbulb",
+        link: "jsjyl/",
+      },
+      {
+        text: "计算机网络",
+        icon: "lightbulb",
+        link: "jsjwl/",
+      },
+    ],
+  },
+]);
---
+++ src/README.md
@@ -0,0 +1,32 @@
+---
+home: true
+icon: home
+title: 主页
+heroImage: https://q.qlogo.cn/headimg_dl?dst_uin=1586049354&spec=640&img_type=jpg
+bgImage: https://theme-hope-assets.vuejs.press/bg/10-light.svg
+bgImageDark: https://theme-hope-assets.vuejs.press/bg/3-dark.svg
+heroFullScreen: true
+bgImageStyle:
+  background-attachment: fixed
+heroText: zbh的考研复习笔记
+tagline: 希望人没事
+actions:
+  - text: 考研数学
+    icon: lightbulb
+    link: ./math/
+    type: primary
+  - text: 考研政治
+    icon: lightbulb
+    link: ./political/
+    type: primary
+  - text: 考研英语
+    icon: lightbulb
+    link: ./english/
+    type: primary
+  - text: 408
+    icon: lightbulb
+    link: ./408/
+    type: primary
+copyright: false
+footer: 使用 <a href="https://theme-hope.vuejs.press/zh/" target="_blank">VuePress Theme Hope</a> 主题 | MIT 协议, 版权所有 © 2019-present Mr.Hope
+---
